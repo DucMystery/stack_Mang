f9dacefdf9d3da8827c0d1d8d55b10a419d0b3cf
DucMystery <58973644+DucMystery@users.noreply.github.com>
2020-04-22 22:20:27 +0700

    Hoan thanh

---
+++ src/MyStack.java
@@ -0,0 +1,42 @@
+public class MyStack {
+
+    private int[] arr;
+    private int size;
+    private int index=0;
+
+    public MyStack(int size){
+        this.size =size;
+        arr = new int[size];
+    }
+
+    boolean isFull(){
+        if (index==size)
+            return true;
+        return false;
+    }
+
+    boolean isEmpty(){
+        if (index==0)
+            return true;
+        return false;
+    }
+
+    void push(int element){
+        if (isFull()){
+            throw new StackOverflowError("Stack is full !");
+        }
+        arr[index] =element;
+        index++;
+    }
+
+    int pop() throws Exception{
+        if (isEmpty()){
+            throw new Exception("Stack is null");
+        }
+        return arr[--index];
+    }
+    int getSize(){
+        return index;
+    }
+
+}
