836146ea32d8ffdcb4fe16e2a986dcf69fe27a6d
DucMystery <58973644+DucMystery@users.noreply.github.com>
2020-04-23 09:19:55 +0700

    Hoan thanh bai tap chuyen doi nhi phan

--- src/Test.java
+++ src/Test.java
@@ -13,13 +13,46 @@ public class Test {
         }
         System.out.println("\n");
         int index1 = 0;
-        int myArr[] = new int[7];
+        int myArr[] = new int[5];
         while (!stack.isEmpty()) {
             myArr[index1] = stack.pop();
             System.out.print(myArr[index1]+"\t");
             index1++;
         }
+
+        System.out.println("==========================");
+
+
+        MyStack stack1 = new MyStack(5);
+        stack1.push(111);
+        swapNhiPhan(111);
+
     }
+
+   public static void swapNhiPhan(int number){
+        int myArr[] =new int[100];
+        int index =0;
+        if (number<=0){
+            return;
+        }
+        while (number!=0){
+            if (number%2!=0){
+                number=number/2;
+                myArr[index] =1;
+                index++;
+            }else if (number%2==0 && number>=2){
+                number =number/2;
+                myArr[index] = 0;
+                index++;
+            }else if (number<2){
+                myArr[index]=1;
+                return;
+            }
+        }
+        for (int i=0;i<index;i++){
+            System.out.print(""+myArr[i]);
+        }
+   }
 
 }
 
